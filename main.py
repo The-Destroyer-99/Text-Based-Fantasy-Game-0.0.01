da5434caf3696568afb062b7a8b52c0c643d90fd
Ayden Quinn <aydenquinn072@gmail.com>
2021-03-08 17:35:51 +0000

    Updating Crap

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from time import sleep as slp
-from os import system as stm
 import os
 import random
 import sys
 import time
+from colorama import init as ini
+from colorama import Fore as txt
+from colorama import Back as bhnd
+from colorama import Style
+import colorama
+
+ini(autoreset=True)
+
+
 
 # variable list
 # fancylineshort
 # fancylinelong
 # startmenuinput
 # typehere
-# username
+# name
 # credits
 # backstoryp1 to p8
 # BS1 To BS8
@@ -18,8 +26,33 @@ import time
 # loadingtime
 # LTfloat
 # inlval
+# title
+#  
+
+
+
+
+
+title = """ _____ _            _   _           _   
+|_   _| |          | \ | |         | |  
+  | | | |__   ___  |  \| | _____  _| |_ 
+  | | | '_ \ / _ \ | . ` |/ _ \ \/ / __|
+  | | | | | |  __/ | |\  |  __/>  <| |_ 
+  \_/ |_| |_|\___| \_| \_/\___/_/\_\\\\__|
+                                        
+                                        
+          _                    _        
+         | |                  | |       
+         | |     _____   _____| |       
+         | |    / _ \ \ / / _ \ |       
+         | |___|  __/\ V /  __/ |       
+         \_____/\___| \_/ \___|_|       
+                                        
+                                        """
 
 inval = "Invalid Input"
+
+
 
 def fasttype(a):
   for b in a:
@@ -53,8 +86,13 @@ TheDestroyer99|Ayden Q.
 
 -\__/-+-=~~~~=-+-\__/-
 
-Lead Helper\\Game Dev:
+Lead Helper\Game Dev:
 Christopher Dai
+
+-\__/-+-=~~~~=-+-\__/-
+
+Backstory Writer
+TheModerator
 """
 
 typehere = ">> "
@@ -74,6 +112,7 @@ BS5 = int(0)
 BS6 = int(0)
 BS7 = int(0)
 BS8 = int(0)
+
 
 
 loadingtime = random.randint(1, 4)
@@ -102,6 +141,9 @@ print("Done!!!")
 loadingtime = random.randint(1, 4)
 slp(loadingtime)
 os.system("clear")
+print(txt.WHITE + title)
+print("\n\n\n\nPress Enter To Start")
+input()
 
 while True:
 	slp(2)
@@ -148,10 +190,17 @@ How To Play
 		LTfloat = random.random()
 		slp(LTfloat)
 		os.system("clear")
-		print("Welcome!!!\n")
 		break
 	else:
 		fasttype(inval)
+kindaslowtype("""??? > wake up....
+Wake Up!...
+WAKE UP!!!!!
+""")
+name = kindaslowtype(input(""" 
+
+"""+ typehere))
+
 
 midtype('''
 Would You Like To Know The Backstory?
@@ -165,98 +214,103 @@ backstory_yn = input(typehere)
 
 if backstory_yn.lower() == "yes" :
 	BS1 += 1
+	if BS1 == 1 :
+		os.system("clear")
+		kindaslowtype(backstoryp1)
+		pass
+	else :
+		pass
+
+	print("\n\nPress Enter To Continue\n\n")
+	input()
+	BS2 += 1
+
+	if BS2 == 1 :
+		os.system("clear")
+		kindaslowtype(backstoryp2)
+		pass
+	else :
+		pass
+
+	print("\n\nPress Enter To Continue\n\n")
+	input()
+	BS3 += 1
+
+
+	if BS3 == 1 :
+		os.system("clear")
+		kindaslowtype(backstoryp3)
+		pass
+	else :
+		pass
+
+	print("\n\nPress Enter To Continue\n\n")
+	input()
+	BS4 += 1
+
+
+	if BS4 == 1 :
+		os.system("clear")
+		kindaslowtype(backstoryp4)
+		pass
+	else :
+		pass
+
+	print("\n\nPress Enter To Continue\n\n")
+	input()
+	BS5 += 1
+
+
+	if BS5 == 1 :
+		os.system("clear")
+		kindaslowtype(backstoryp5)
+		pass
+	else :
+		pass
+
+	print("\n\nPress Enter To Continue\n\n")
+	input()
+	BS6 += 1
+
+
+	if BS6 == 1 :
+		os.system("clear")
+		kindaslowtype(backstoryp6)
+		pass
+	else :
+		pass
+
+	if input("")  == """
+""" :
+		BS7 += 1
+	else :
+		pass
+
+	if BS7 == 1 :
+		os.system("clear")
+		kindaslowtype(backstoryp7)
+		pass
+	else :
+		pass
+
+	if input("")  == """
+""" :
+		BS8 += 1
+	else :
+		pass
+
+	if BS8 == 1 :
+		os.system("clear")
+		kindaslowtype(backstoryp8)
+		pass
+	else :
+		pass
 elif backstory_yn.lower() == "no" :
 	pass
 else :
 	print(inval)
 
-if BS1 == 1 :
-	os.stm("clear")
-	kindaslowtype(backstoryp1)
-else :
-	pass
 
-if input("") == """
-""" :
-	BS2 += 1
-else :
-	pass
 
-if BS2 == 1 :
-	os.stm("clear")
-	kindaslowtype(backstoryp2)
-else :
-	pass
 
-if input("")  == """
-""" :
-	BS3 += 1
-else :
-	pass
-
-if BS3 == 1 :
-	os.stm("clear")
-	kindaslowtype(backstoryp3)
-else :
-	pass
-
-if input("")  == """
-""" :
-	BS4 += 1
-else :
-	pass
-
-if BS4 == 1 :
-	os.stm("clear")
-	kindaslowtype(backstoryp4)
-else :
-	pass
-
-if input("")  == """
-""" :
-	BS5 += 1
-else :
-	pass
-
-if BS5 == 1 :
-	os.stm("clear")
-	kindaslowtype(backstoryp5)
-else :
-	pass
-
-if input("")  == """
-""" :
-	BS6 += 1
-else :
-	pass
-
-if BS6 == 1 :
-	os.stm("clear")
-	kindaslowtype(backstoryp6)
-else :
-	pass
-
-if input("")  == """
-""" :
-	BS7 += 1
-else :
-	pass
-
-if BS7 == 1 :
-	os.stm("clear")
-	kindaslowtype(backstoryp7)
-else :
-	pass
-
-if input("")  == """
-""" :
-	BS8 += 1
-else :
-	pass
-
-if BS8 == 1 :
-	os.stm("clear")
-	kindaslowtype(backstoryp8)
-else :
-	pass
 
